fd853ae8470faa81f0ddc18276f15ce35b2fd88f
MichaelKirkaldyV <vmichaelkirkaldy@live.com>
2021-06-17 14:36:11 -0500

    check permutations algo

---
+++ strings_.py
@@ -0,0 +1,27 @@
+#Write a function to check whether two given strings are Permutation of each other or not. A Permutation of a string is another string that contains same characters, only the order of characters can be different. For example, “abcd” and “dabc” are Permutation of each other.
+
+def checkPerm(str1, str2):
+    # Check to see if the strings are equal, if not they aren't permutations. Permutations have equal characters!
+    s1 = len(str1)
+    s2 = len(str2)
+    if s1 != s2:
+        return False
+    
+    # Wwe should sort the strings and see if each character is equal to another. If so then we have permutations.
+    a = sorted(str1)
+    b = sorted(str2)
+    print(a, b)
+
+    # Check if the character of the sorted string are equal, if so then we have a permutation.
+    for x in range(0, s1):
+        if a[x] != b[x]:
+            return False
+
+    # If all test cases pass then it is a permutation.       
+    return True
+
+
+a = "abddd"
+b = "adddb"
+print(checkPerm(a, b))
+
